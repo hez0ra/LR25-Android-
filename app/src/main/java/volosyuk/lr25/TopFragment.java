a504438de515c064bcbcbd87f0f357e1e07ad4c1
hez0ra <feleepen@gmail.com>
2024-11-04 10:55:23 +0300

    lr30

--- app/src/main/java/volosyuk/lr25/TopFragment.java
+++ app/src/main/java/volosyuk/lr25/TopFragment.java
@@ -1,2 +1,16 @@
-package volosyuk.lr25;public class TopFragment {
+package volosyuk.lr25;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TopFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
 }
